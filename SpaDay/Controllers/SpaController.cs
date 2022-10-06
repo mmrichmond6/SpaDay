a405cc387a07fdf9817b22f2e16ba62c747bb6f8
mmrichmond6 <mmrichmond6@gmail.com>
2022-10-05 20:00:37 -0500

    Done

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -10,6 +10,8 @@ namespace SpaDay.Controllers
 {
     public class SpaController : Controller
     {
+        public string name { get; set; }
+        
         public bool CheckSkinType(string skinType, string facialType)
         {
 
@@ -42,6 +44,9 @@ namespace SpaDay.Controllers
         [Route("/spa")]
         public IActionResult Menu(string name, string skintype, string manipedi)
         {
+            ViewBag.name = name;
+            ViewBag.skintype = skintype;
+            ViewBag.manipedi = manipedi;
             List<string> facials = new List<string>()
             {
                 "Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"
@@ -55,6 +60,7 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+            ViewBag.appropriateFacials = appropriateFacials;
             return View();
         }
 
