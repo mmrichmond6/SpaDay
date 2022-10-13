0b96adb69118166d7dc1d4c169f7576282dffdb8
mmrichmond6 <mmrichmond6@gmail.com>
2022-10-13 09:13:05 -0500

    Progress made

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.Extensions.Logging;
+using SpaDay.Models;
+using System.Xml.Linq;
 
 namespace SpaDay.Controllers
 {
@@ -7,6 +11,26 @@ namespace SpaDay.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+
+            if (verify == newUser.Password)
+            {
+                ViewBag.name = newUser.Username;
+                return View("Index");
+            }
+            return Redirect("/user/add");
         }
     }
 }
--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -2,5 +2,22 @@
 {
     public class User
     {
+
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
     }
 }
